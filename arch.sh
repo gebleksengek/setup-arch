19885eda0c0b0d1791e61e1d32f2552d42944b14
Juhun Lee <hello@rangho.me>
2025-05-08 22:12:16 +0900

    feat: use /bin/bash instead of /bin/sh

--- arch.sh
+++ arch.sh
@@ -14,4 +14,4 @@ export | sudo tee "$rootfs/bounce/env.sh" >/dev/null
 exec sudo "$rootfs/bin/arch-chroot" "$rootfs" \
 	/bin/su "$user" \
 		/bin/sh -lc ". /bounce/env.sh; cd '$oldpwd' 2>/dev/null; exec \"\$@\"" -- \
-			/bin/sh -eo pipefail "$@"
+			/bin/bash -eo pipefail "$@"
